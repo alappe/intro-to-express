6c794faf2c348125d67e9380cde7ffcaa8b62ee1
Andreas Lappe <nd@off-pist.de>
2013-11-28 14:30:35 +0100

    Add some log messages and fix timing

--- app.js
+++ app.js
@@ -15,24 +15,28 @@ app.use(express.bodyParser());
 app.use(express.static('public'));
 
 app.get('/', function(req, res) {
+	console.log('GET /');
 	client.increment('homepage');
 	res.render('index',{title:"My Blog", entries:blogEngine.getBlogEntries()});
 });
 
 app.get('/about', function(req, res) {
+	console.log('GET /about');
 	client.increment('about');
 	res.render('about', {title:"About Me"});
 });
 
 app.get('/article/:id', function(req, res) {
+	console.log('GET /article/'+req.params.id);
 	client.increment('article');
 	client.increment('article-'+req.params.id);
 	var base = Date.now();
 	setTimeout(function() {
-		client.timing('db-response-time', base-Date.now());
+		client.timing('db-response-time', Date.now() - base);
+		console.log('db-response-time ' + (Date.now() - base) + 'ms');
 		var entry = blogEngine.getBlogEntry(req.params.id);
 		res.render('article',{title:entry.title, blog:entry});
 	}, Math.random() * 1000);
 });
 
-app.listen(3000);
+app.listen(3333);
